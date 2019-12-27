673d978d2f4d850efad84ebcd83f237c2a94eee4
unknown <jussi.kaatiala@gmail.com>
2019-12-27 16:54:08 +0200

    Can load and display indexfile and css

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const http = require("http")
 const fs = require("fs");
 
-var port = 80;
-
+const port = 80;
+const indexfile = "build/index.html";
+const cssfile = "build/style.css";
 
 function on_request(request, response) {
 
    if (request.method == "GET") {
       if (request.url == "/") {
-         response.write("hello world!");
+         // indexfile
+
+         var filecontents = fs.readFileSync(indexfile, {encoding: "utf8"});
+         response.writeHead(200, {"Content-type" : "text/html"});
+         response.write(filecontents);
+         response.end();
+      } else if (request.url == "style.css") {
+         // css
+
+         var filecontents = fs.readFileSync(cssfile, {encoding: "utf8"});
+         response.writeHead(200, {"Content-type" : "text/css"});
+         response.write(filecontents);
+         response.end();
+      } else {
+         // all other pages
+
+         response.writeHead(404);
+         response.write("File not found!");
          response.end();
       }
    }
-
 }
-
-
 
 http.createServer(on_request).listen(port);
