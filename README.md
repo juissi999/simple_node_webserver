d513a2690aabf4fd5abf8246a098ba4eb5e5fe47
unknown <jussi.kaatiala@gmail.com>
2019-12-27 18:38:39 +0200

    Can host a simple project with css, js, png

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # simple_node_webserver
 A very simple node server to easily to host front-end projects
+
+### NOTE!
+This server is not safe to use in public web. It is for prototyping purposes on closed environment. For example paths are not checked. Use with care.
--- server.js
+++ server.js
@@ -1,33 +1,56 @@
 const http = require("http")
 const fs = require("fs");
+const path = require("path")
 
 const port = 80;
+const builddir = "build";
 const indexfile = "build/index.html";
 const cssfile = "build/style.css";
 
 function on_request(request, response) {
 
    if (request.method == "GET") {
+      let filetype = request.url.split(".")[1];
       if (request.url == "/") {
          // indexfile
 
-         var filecontents = fs.readFileSync(indexfile, {encoding: "utf8"});
+         var filecontents = fs.readFileSync(path.join(builddir, "index.html"), {encoding: "utf8"});
          response.writeHead(200, {"Content-type" : "text/html"});
          response.write(filecontents);
          response.end();
-      } else if (request.url == "style.css") {
+      } else if (filetype == "css") {
          // css
 
-         var filecontents = fs.readFileSync(cssfile, {encoding: "utf8"});
-         response.writeHead(200, {"Content-type" : "text/css"});
-         response.write(filecontents);
-         response.end();
-      } else {
-         // all other pages
+         fs.readFile(path.join(builddir, request.url), (err, data) => {
+            if (err) {
+               return;
+            }
+            response.writeHead(200, {"Content-type" : "text/css"});
+            response.write(data);
+            response.end();
+         });
+      } else if (filetype == "js") {
+         // javascript
 
-         response.writeHead(404);
-         response.write("File not found!");
-         response.end();
+         fs.readFile(path.join(builddir, request.url), (err, data) => {
+            if (err) {
+               return;
+            }
+            response.writeHead(200, {"Content-type" : "application/javascript"});
+            response.write(data);
+            response.end();
+         });
+      } else if (filetype == "png") {
+         // png images
+
+         fs.readFile(path.join(builddir, request.url), (err, data) => {
+            if (err) {
+               return;
+            }
+            response.writeHead(200, {"Content-type" : "image/png"});
+            response.write(data);
+            response.end();   
+         });
       }
    }
 }
